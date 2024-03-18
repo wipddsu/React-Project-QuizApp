32ced207641efe411ae4aef0ba97000214924073
wipddsu <rlaalstn8158@naver.com>
2024-03-18 13:52:14 +0900

    Quiz 컴포넌트 코드 작성

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,30 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswer, setUserAnswer] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswer.length;
+
+  function handleSelectAnswer(selectedAndswer) {
+    setUserAnswer((prevAnswers) => {
+      return [...prevAnswers, selectedAndswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
