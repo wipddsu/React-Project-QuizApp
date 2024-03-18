b62c81a6b248c6e842d4d94a98be2dced56cefa6
wipddsu <rlaalstn8158@naver.com>
2024-03-18 14:39:35 +0900

    퀴즈 선지 랜덤 출력, 퀴즈 종료 화면 출력

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 
 import QUESTIONS from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
 
   const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function answersShuffle(answers) {
+    for (let i = answers.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+
+    return answers;
+  }
 
   function handleSelectAnswer(selectedAndswer) {
     setUserAnswer((prevAnswers) => {
@@ -13,12 +24,23 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = answersShuffle([...QUESTIONS[activeQuestionIndex].answers]);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
